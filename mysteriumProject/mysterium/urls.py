99998ac926d014ae29a95d020c488dda50af715b
MertUnlu-SWE <mert.unlu@std.bogazici.edu.tr>
2024-11-16 16:48:59 +0300

    TO DO: edit post page load error issue

--- mysteriumProject/mysterium/urls.py
+++ mysteriumProject/mysterium/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('index/', views.index, name='index'),
     path('postCreation/', views.post_creation, name='postCreation'),
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),  # Place edit_post before vote_post
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('post/<int:post_id>/<str:vote_type>/', views.vote_post, name='vote_post'),
     path('comment/<int:comment_id>/<str:vote_type>/', views.vote_comment, name='vote_comment'),
-    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
     path('search/', views.search_tags, name='search_tags'),
     path('fetch_wikidata/', views.fetch_wikidata, name='fetch_wikidata'),
 ]
