b1276b6b50af0c527bced9ef58cad71f8ae86052
MertUnlu-SWE <mert.unlu@std.bogazici.edu.tr>
2024-11-16 16:20:31 +0300

    wsgi.py configuration for image serving

--- mysteriumProject/mysteriumProject/wsgi.py
+++ mysteriumProject/mysteriumProject/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.conf import settings
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysteriumProject.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
