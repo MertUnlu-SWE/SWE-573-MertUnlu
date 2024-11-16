df86462ff6b3602fcaee3b651817061f27cf9310
MertUnlu-SWE <mert.unlu@std.bogazici.edu.tr>
2024-11-16 18:26:38 +0300

    Update wsgi.py

--- mysteriumProject/mysteriumProject/wsgi.py
+++ mysteriumProject/mysteriumProject/wsgi.py
@@ -15,4 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysteriumProject.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(settings.BASE_DIR, "media"), prefix="/media/")
+application = WhiteNoise(application, root=settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
